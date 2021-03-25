b15da638d7a02bf731073df4a18a1e81ccafe992
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-03-24 20:18:50 -0700

    make email input component

---
+++ src/components/inputs/EmailInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-input
+    label="Email"
+    type="email"
+    :rules="[val => !!val || 'Email is required']"
+    class="q-px-sm q-pb-lg"
+    dense
+  />
+</template>
+
+<script>
+export default {
+  name: 'EmailInput'
+}
+</script>
