6eac4b84acbd25c6c82f7fc23fef3f77795745b6
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-03-24 20:19:12 -0700

    create send reset email form

---
+++ src/components/forms/SendResetEmailForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-form
+    class="fixed-center"
+    id="send-email"
+    @submit="sendEmail"
+  >
+    <q-card
+      bordered
+      class="q-mx-auto"
+    >
+      <email-input 
+        v-model="email"
+      />
+    </q-card>
+    <submit-button
+      :loading="showLoadingButton"
+    />
+  </q-form>
+</template>
+
+<script>
+import { useQuasar } from 'quasar';
+import SubmitButton from '../buttons/SubmitButton';
+import EmailInput from '../inputs/EmailInput';
+import { ref } from 'vue';
+
+export default {
+  name: 'SendResetEmailForm',
+  components: {
+    SubmitButton,
+    EmailInput
+  },
+  setup() {
+    const $q = useQuasar();
+    let showLoadingButton = ref(false);
+    let email = ref(null);
+
+    async function sendEmail() {
+      showLoadingButton.value = true;
+    }
+  }
+}
+</script>
