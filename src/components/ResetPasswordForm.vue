a68f62e52ea962a6ae6028acc4a9257e40a3aaa8
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-03-24 19:30:40 -0700

    make reset password form component

---
+++ src/components/ResetPasswordForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-form
+    class="fixed-center"
+    id="reset-password"
+    @submit="resetPassword"
+  >
+    <q-card
+      bordered
+      class="q-mx-auto"
+    >
+      <password-input
+        v-model="password"
+      />
+      <password-input
+        v-model="passwordConfirm"
+      />
+    </q-card>
+    <submit-button
+      :loading="showLoadingButton"
+    />
+  </q-form>
+</template>
+
+<script>
+import PasswordInput from './PasswordInput';
+import SubmitButton from './SubmitButton';
+import { ref } from 'vue';
+import { api } from 'boot/axios';
+import { Router } from '../router/index';
+import { useQuasar } from 'quasar';
+
+export default {
+  name: 'ResetPasswordForm',
+  components: {
+    PasswordInput,
+    SubmitButton
+  },
+  setup() {
+    const $q = useQuasar();
+    let showLoadingButton = ref(false);
+    let password = ref(null);
+    let passwordConfirm = ref(null);
+
+    async function resetPassword() {
+      showLoadingButton.value = true;
+    }
+
+    return {
+      showLoadingButton,
+      password,
+      passwordConfirm,
+      resetPassword
+    }
+  }
+}
+</script>
