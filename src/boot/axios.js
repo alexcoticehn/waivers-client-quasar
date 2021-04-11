8ffaaaf2d79ee677cd2def3587273f8a60cd2e7d
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-04-10 20:54:15 -0700

    always send requests withCreds

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const api = axios.create({ baseURL: 'https://jailors.herokuapp.com/api/' })
 
 export default boot(({ app }) => {
+  api.defaults.withCredentials = true;
   app.config.globalProperties.$api = api
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
