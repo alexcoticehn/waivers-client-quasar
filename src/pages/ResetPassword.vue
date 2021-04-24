8b51f0df30eb0555f0a6ba25ffb31b0ebe74a3ea
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-04-23 20:03:45 -0700

    add form to reset password page

--- src/pages/ResetPassword.vue
+++ src/pages/ResetPassword.vue
@@ -1,3 +1,16 @@
 <template>
-  <h4>Reset password</h4>
+  <q-page>
+    <div class="row justify-center">
+      <reset-password-form />
+    </div>
+  </q-page>
 </template>
+
+<script>
+import ResetPasswordForm from '../components/forms/ResetPasswordForm'
+export default {
+  components: {
+    ResetPasswordForm
+  }
+}
+</script>
