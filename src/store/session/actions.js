bdc5fbf40ea6672e042329c2116cbf2dba57df6a
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-04-26 15:44:58 -0700

    remove commented code from actions

--- src/store/session/actions.js
+++ src/store/session/actions.js
@@ -1,17 +1,5 @@
 import { api } from 'boot/axios';
-import { Loading } from 'quasar';
 
 export function verifyToken ({ commit }) {
     return api.get('auth/token/verify', {});
-    /*
-    .then(() => {
-        commit('setIsAuthenticated', true);
-        this.$router.replace({name: 'MyRoster'});
-        Loading.hide();
-    })
-    .catch(() => {
-        commit('setIsAuthenticated', false);
-        Loading.hide();
-    })
-    */
 }
