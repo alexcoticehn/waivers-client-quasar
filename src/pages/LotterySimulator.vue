5bc1bc4adad8baf741e907898b5511f35bcfa5ec
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-06 21:13:30 -0700

    oops wrong file

--- src/pages/LotterySimulator.vue
+++ src/pages/LotterySimulator.vue
@@ -18,6 +18,7 @@
       <lottery-odds-table
         :rows='rows'
         :columns='columns'
+        :visible-columns="['team', 'owner', 'odds']"
       />
     </div>
   </q-page>
@@ -30,20 +31,21 @@ import SubmitButton from "../components/buttons/SubmitButton"
 const columns = [
     { name: 'team', label: 'Team', field: row => row.team, align: 'left' },
     { name: 'owner', label: 'Owner', field: 'owner', align: 'left' },
-    { name: 'odds', label: 'Chance of 1st overall pick', field: row => row.odds, sortable: true, format: val => `${val}%`, align: 'left' }
+    { name: 'odds', label: 'Chance of 1st overall pick', field: row => row.odds, sortable: true, format: val => `${val}%`, align: 'left' },
+    { name: 'rangeMax', label: 'rangeMax', field: row => row.rangeMax, required: false }
 ]
 
 const rows = [
-    { team: "Don't Toews Me, Bro", owner: "Alex Cotic-Ehn", odds: 40 },
-    { team: "Bustin' Pumpkins", owner: "Michael Winter", odds: 20 },
-    { team: "McJesus!", owner: "Jimmy Ghuman", odds: 16 },
-    { team: "Big Money", owner: "Peter Bohdal", odds: 12 },
-    { team: "Wet Dick Carters", owner: "Ian Carter", odds: 7 },
-    { team: "Double Pennertration", owner: "Eric Wallin", odds: 3 },
-    { team: "Team Too Good", owner: "Nathan Alvarez", odds: 2 },
-    { team: "Dude, Where's Makar?", owner: "Chris Littomericzky", odds: 0 },
-    { team: "Wheeler? I Hardly Knew Her", owner: "Harrison Brown", odds: 0 },
-    { team: "Don't Be Saad", owner: "Dimitri Filipovic", odds: 0 }
+    { team: "Don't Toews Me, Bro", owner: "Alex Cotic-Ehn", odds: 40, rangeMax: 40 },
+    { team: "Bustin' Pumpkins", owner: "Michael Winter", odds: 20, rangeMax: 60 },
+    { team: "McJesus!", owner: "Jimmy Ghuman", odds: 16, rangeMax: 76 },
+    { team: "Big Money", owner: "Peter Bohdal", odds: 12, rangeMax: 88 },
+    { team: "Wet Dick Carters", owner: "Ian Carter", odds: 7, rangeMax: 95 },
+    { team: "Double Pennertration", owner: "Eric Wallin", odds: 3, rangeMax: 98 },
+    { team: "Team Too Good", owner: "Nathan Alvarez", odds: 2, rangeMax: 100 },
+    { team: "Dude, Where's Makar?", owner: "Chris Littomericzky", odds: 0, rangeMax: 101 },
+    { team: "Wheeler? I Hardly Knew Her", owner: "Harrison Brown", odds: 0, rangeMax: 101 },
+    { team: "Don't Be Saad", owner: "Dimitri Filipovic", odds: 0, rangeMax: 101 }
 ]
 
 export default {
@@ -59,7 +61,14 @@ export default {
     },
     methods: {
         runLottery() {
-            console.log('test');
+            const randomVal = Math.random() * 100;
+            let winningTeam = {rangeMax: 101};
+            rows.forEach((team) => {
+                if (randomVal < team.rangeMax && team.rangeMax < winningTeam.rangeMax) {
+                    winningTeam = team;
+                }
+            })
+            console.log(winningTeam.team);
         }
     }
 }
