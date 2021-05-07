c298a7b37425fbb27b770afb1feef173bf8f2b62
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-06 19:09:01 -0700

    add different material icon sets

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,6 +47,9 @@ module.exports = configure(function (/* ctx */) {
 
       'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
+      'material-icons-outlined',
+      'material-icons-round',
+      'material-icons-sharp'
     ],
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
