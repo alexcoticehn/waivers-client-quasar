24e01dc26effbf674c2fe6aae6d468557f0f2a31
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-06 20:51:50 -0700

    made lottery odds component table

---
+++ src/components/tables/LotteryOddsTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-table 
+    title='Lottery Odds'
+    :rows='rows'
+    :columns='columns'
+    row-key='team'
+    hide-pagination
+    :pagination='pagination'
+  />
+</template>
+
+<script>
+
+const pagination = {
+    sortBy: 'odds',
+    descending: true,
+    rowsPerPage: 0
+}
+
+export default {
+    name: 'LotteryOddsTable',
+    data() {
+        return {
+            pagination: pagination
+        }
+    },
+    props: {
+        columns: Object,
+        rows: Object
+    }
+}
+</script>
