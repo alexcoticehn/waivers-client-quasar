6d5ea79b00598e6af21664d9c56cf79ea6939336
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-12 22:01:22 -0700

    remove home test, add success and failed login tests

---
+++ test/cypress/integration/landing.test.js
@@ -0,0 +1,49 @@
+// <reference path="cypress" />
+/// <reference path="../support/index.d.ts" />
+
+// Use `cy.dataCy` custom command for more robust tests
+// See https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
+
+// ** This file is an example of how to write Cypress tests, you can safely delete it **
+
+// This test will pass when run against a clean Quasar project
+describe('Landing Page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should show correct title', () => {
+    cy.title().should('include', "Sailor Jerry's");
+  });
+
+  it('should login successfully', () => {
+    cy.intercept('POST', '/users/login', {
+      statusCode: 200
+    });
+    cy.get('[data-cy=username-input]')
+      .type('acoticehn');
+    cy.get('[data-cy=password-input]')
+      .type('password');
+    cy.get('[data-cy=submit-btn]')
+      .click();
+    cy.url().should('eql', Cypress.config().baseUrl + 'roster');
+  });
+
+  it(('should reject failed login'), () => {
+    cy.intercept('POST', '/users/login', {
+      statusCode: 401,
+      body: {
+        errors: {
+          message: 'Login failed'
+        }
+      }
+    })
+    cy.get('[data-cy=username-input]')
+      .type('acoticehn');
+    cy.get('[data-cy=password-input]')
+      .type('password');
+    cy.get('[data-cy=submit-btn]')
+      .click();
+    cy.url().should('eql', Cypress.config().baseUrl);
+  });
+});
