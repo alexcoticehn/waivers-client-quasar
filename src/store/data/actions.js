29d7ae90b9189a4f00995b7774fe706bbcfb3f7b
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-11-15 21:05:49 -0800

    add logic into getTeams action to only grab data if we haven't already done so

--- src/store/data/actions.js
+++ src/store/data/actions.js
@@ -1,5 +1,21 @@
 import { api } from 'boot/axios';
 
-export function getTeams () {
-    return api.get('teams');
+export function getTeams (context) {
+    if (context.state.teams.length != 0) {
+        return context.state.teams;
+    }
+    return api.get('teams')
+        .then((res) => {
+            let teams = [];
+            res.data.teams.forEach(element => {
+                let team_data = {};
+                team_data.teamId = element._id;
+                team_data.teamName = element.name;
+                team_data.prospectTeamName = element.name_prospect;
+                team_data.ownerName = element.owner.firstname + " " + element.owner.lastname;
+                teams.push(team_data);
+            })
+            context.commit('setTeams', teams);
+            return context.state.teams;
+        })
 }
