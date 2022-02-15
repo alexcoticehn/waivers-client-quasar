b104d7d0bb8fa9b34f0917c2a860ef5622546642
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-02-14 19:50:10 -0800

    add integer input to form and form to page

---
+++ src/components/forms/AddDraftPickForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <integer-input />
+    </div>
+</template>
+
+<script>
+import IntegerInput from "../inputs/number/IntegerInput.vue";
+
+export default {
+    components: {
+        IntegerInput
+    }
+}
+</script>
--- src/pages/AddDraftPicks.vue
+++ src/pages/AddDraftPicks.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-        <h1>Add Draft Picks</h1>
-    </div>
+    <q-page>
+        <div>
+            <h1>Add Draft Picks</h1>
+            <add-draft-pick-form />
+        </div>
+    </q-page>
 </template>
 
 <script>
+import AddDraftPickForm from "src/components/forms/AddDraftPickForm.vue";
+
+export default {
+    components: {
+        AddDraftPickForm
+    }
+}
 
 </script>
